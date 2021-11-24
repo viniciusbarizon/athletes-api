91b7a2435034ee247b440f5e66aa2532554777e7
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-11-24 19:53:21 +0000

    migration: CreateAthletesTable

---
+++ database/migrations/2021_11_24_194245_create_athletes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAthletesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('athletes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->date('birth_date')->index();
+            $table->string('email', 320)->unique()->index();
+            $table->unsignedTinyInteger('height')->index();
+            $table->string('name', 747)->index();
+            $table->unsignedInteger('nif')->unique()->index();
+            $table->unsignedInteger('telephone')->index();
+            $table->unsignedSmallInteger('weight')->index();
+
+            $table->timestamp('created_at')->nullable(false)->useCurrent();
+            $table->timestamp('updated_at')->nullable(false)->useCurrent();
+            $table->softDeletes();
+
+            $table->foreignId('sport_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('athletes');
+    }
+}
