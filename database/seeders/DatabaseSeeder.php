75cbf01ce77c3ef3a68e36c57ea3f1c62ae9f7bd
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-11-24 23:49:23 +0000

    DatabaseSeeder: order

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            SportSeeder::class,
             FootballPositionSeeder::class,
-            KarateBeltSeeder::class
+            KarateBeltSeeder::class,
+            SportSeeder::class,
         ]);
     }
 }
