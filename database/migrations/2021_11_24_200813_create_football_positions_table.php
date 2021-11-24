d919b2351874ee130ade8592b78ce182e3617381
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-11-24 20:10:28 +0000

    migration: CreateFootballPositionsTable

---
+++ database/migrations/2021_11_24_200813_create_football_positions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFootballPositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('football_positions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('position', 20)->unique();
+
+            $table->timestamp('created_at')->nullable(false)->useCurrent();
+            $table->timestamp('updated_at')->nullable(false)->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('football_positions');
+    }
+}
