92c997567b32802793b0a0e68749cdf207548525
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-11-25 21:04:55 +0000

    DestroyActionTest

--- tests/Unit/DestroyActionTest.php
+++ tests/Unit/DestroyActionTest.php
@@ -6,6 +6,9 @@ use App\Actions\DestroyAction;
 use App\Http\Resources\FootballPositionResource;
 use App\Models\FootballPosition;
 
+use Error;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use Tests\TestCase;
@@ -18,10 +21,14 @@ class DestroyActionTest extends TestCase
 
     private int $id = 1;
     private string $model = FootballPosition::class;
+    private string $resource = FootballPositionResource::class;
 
-    public function test_found_must_soft_deleted()
+    public function test_found_must_soft_deleted(): void
     {
-        $this->actionExecute();
+        $this->assertEquals(
+            $this->resource,
+            get_class($this->actionExecute())
+        );
 
         $this->assertDatabaseMissing('football_positions', [
             'id' => $this->id,
@@ -29,12 +36,41 @@ class DestroyActionTest extends TestCase
         ]);
     }
 
+    public function test_not_found_must_return_model_not_found_exception(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        $this->id = 999;
+
+        $this->actionExecute();
+    }
+
+    public function test_model_not_exist_must_return_error_exception(): void
+    {
+        $this->expectException(Error::class);
+        $this->expectExceptionMessage('Class "App\Models\NotExist" not found');
+
+        $this->model = 'App\Models\NotExist';
+
+        $this->actionExecute();
+    }
+
+    public function test_resource_not_exist_must_return_error_exception(): void
+    {
+        $this->expectException(Error::class);
+        $this->expectExceptionMessage('Class "App\Http\Resources\NotExist" not found');
+
+        $this->resource = 'App\Http\Resources\NotExist';
+
+        $this->actionExecute();
+    }
+
     private function actionExecute(): mixed
     {
         return (new DestroyAction)->execute(
             id: $this->id,
             model: $this->model,
-            resource: FootballPositionResource::class
+            resource: $this->resource
         );
     }
 }
