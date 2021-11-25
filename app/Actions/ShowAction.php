0348889169343231dc00038b3731c36f47cb8221
Vinicius Barizon <viniciusbarizon@nauon.com>
2021-11-25 17:12:49 +0000

    ShowAction

--- app/Actions/ShowAction.php
+++ app/Actions/ShowAction.php
@@ -2,10 +2,12 @@
 
 namespace App\Actions;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+
 class ShowAction
 {
-    public function execute(int $id, string $model, string $resource)
+    public function execute(int $id, string $model, string $resource): JsonResource
     {
-
+        return new $resource($model::findOrFail($id));
     }
 }
